4265eab22563f92b67b1e5babf6399706fbc417b
Ehab <ehab.a.gh@gmail.com>
2019-10-29 12:25:53 +0400

    add new code for mob web testing

--- src/test/java/Appium/Appium/Ecommerce3.java
+++ src/test/java/Appium/Appium/Ecommerce3.java
@@ -1,44 +1,119 @@
 package Appium.Appium;
 
 import java.net.MalformedURLException;
+
+import java.util.Set;
+
 import java.util.concurrent.TimeUnit;
 
+
+
 import org.openqa.selenium.By;
-import org.testng.Assert;
+
+import org.openqa.selenium.By.ByClassName;
+
+import org.openqa.selenium.Keys;
+
+import org.openqa.selenium.WebElement;
+
+
+
+import io.appium.java_client.MobileBy;
+
+import io.appium.java_client.TouchAction;
 
 import io.appium.java_client.android.AndroidDriver;
+
 import io.appium.java_client.android.AndroidElement;
 
-public class Ecommerce3 extends firstProject{
-	
-	/*this class to verify two scenarios:
-	 * scrolling down and DDL line 27
-	 * catching the Toast error message and assert line 30 - 33
-	 */
-	
 
-	public static void main(String[] args) throws MalformedURLException {
-		AndroidDriver<AndroidElement> driver=Capabilities();
-		
-		driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
-		
-		driver.findElementById("com.androidsample.generalstore:id/nameField").sendKeys("hello");
-		driver.findElementByXPath("//*[@text='Female']").click();
-		
-		//expand drop down list
-		driver.findElementById("android:id/text1").click();
-		
-		//scroll down and click on specific option
-		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));");
-		driver.findElementByXPath("//*[@text='Argentina']").click();
-		
-		// click on submit button
-		driver.findElementById("com.androidsample.generalstore:id/btnLetsShop").click();
-		
-		//handle toast message
-		String toastMessage = driver.findElement(By.xpath("//android.widget.Toast[1]")).getAttribute("name");
-		System.out.println(toastMessage);
-		Assert.assertEquals(toastMessage, "Please enter your name");
-	}
+
+import static io.appium.java_client.touch.TapOptions.tapOptions;
+
+import static io.appium.java_client.touch.offset.ElementOption.element;
+
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+
+import static java.time.Duration.ofSeconds;
+
+
+
+import junit.framework.Assert;
+
+
+
+public class Ecommerce3 extends firstProject{
+
+
+
+public static void main(String[] args) throws MalformedURLException, InterruptedException {
+
+AndroidDriver<AndroidElement> driver=Capabilities();
+
+     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+     driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Hello");
+
+     driver.hideKeyboard();
+
+     driver.findElement(By.xpath("//*[@text='Female']")).click();
+
+     driver.findElement(By.id("android:id/text1")).click();
+
+     
+
+     driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));");
+
+  //   driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textMatches(\"" + containedText + "\").instance(0))"));     
+
+     driver.findElement(By.xpath("//*[@text='Argentina']")).click();
+
+     driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+
+     
+
+     driver.findElements(By.xpath("//*[@text='ADD TO CART']")).get(0).click();
+
+     driver.findElements(By.xpath("//*[@text='ADD TO CART']")).get(0).click();
+
+    driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+
+Thread.sleep(4000);
+
+//Mobile Gestures
+
+WebElement checkbox=driver.findElement(By.className("android.widget.CheckBox"));
+
+TouchAction t=new TouchAction(driver);
+
+t.tap(tapOptions().withElement(element(checkbox))).perform();
+
+
+
+driver.findElement(By.id("com.androidsample.generalstore:id/btnProceed")).click();
+
+
+
+Thread.sleep(7000);
+
+Set<String> contexts=driver.getContextHandles();
+
+for(String contextName :contexts)
+
+{
+
+System.out.println(contextName);
+
+}
+
+driver.context("WEBVIEW_com.androidsample.generalstore");
+
+driver.findElement(By.name("q")).sendKeys("hello");
+
+driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+
+driver.context("NATIVE_APP");
+
+}
 
 }
